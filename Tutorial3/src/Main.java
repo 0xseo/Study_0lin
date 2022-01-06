c356d1ace5d58e9eb7221401fbb8a8c9916c395e
0xseo <0xseo@yonsei.ac.kr>
2022-01-06 19:12:40 +0900

    Tutorial 3, 4

--- Tutorial3/src/Main.java
+++ Tutorial3/src/Main.java
@@ -1,7 +1,19 @@
 public class Main {
     public static void main(String[] args){
 
+        for (char i = 'a'; i <= 'z'; i++) {
+            System.out.print(i + " ");
+        }
 
+        int a = 200;
+        int b = 0;
+        System.out.println("\n10진수: " + a);
+        System.out.format("8진수: %o\n", a);
+        System.out.format("16진수: %x\n", a);
+
+        String name = "John Doe";
+        System.out.println(name);
+        System.out.println(name.substring(1, 3));
 
     }
 }
